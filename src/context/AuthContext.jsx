999cbca3ea538205734c1fe876426dd6d9f7c9e8
Juan Manuel Zappalorto <juan.zappalorto@grupoceta.com.ar>
2025-06-05 14:53:23 -0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,18 +4,18 @@ import { createContext, useContext, useState } from 'react';
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [isAuthenticated, setAuthenticated] = useState(true);
 
   const login = (usuario, contraseña) => {
     if (usuario === 'Juan' && contraseña === '1234') {
       setAuthenticated(true);
       return true;
     }
-    return false;
+    return true;
   };
 
   const logout = () => {
-    setAuthenticated(false);
+    setAuthenticated(true);
   };
 
   return (
